fef0cc14b42d4c4ddc5e9959ba37107a12ab06ad
Igor Karpuk <i.karpuk@softteco.com>
2020-04-03 10:23:59 +0300

    Second Task

--- src/components/AdaptiveListComponent/AdaptiveListComponent.jsx
+++ src/components/AdaptiveListComponent/AdaptiveListComponent.jsx
@@ -1,29 +1,72 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/label-has-for */
 /* eslint-disable jsx-a11y/label-has-associated-control */
 
-import React from 'react';
+import React, { Fragment, useState } from 'react';
 import styles from './AdaptiveListComponent.scss';
+import { games } from '../../utils/utils';
 
-const AdaptiveListComponent = () => (
-  <main className={styles.mainContent}>
-    <h1>Adaptive list</h1>
-    <div className={styles.listWrapper}>
-      <input type="radio" name="game" id="worldOfTanks" className={styles.inputRadio} />
-      <label className={styles.labelRadio} htmlFor="worldOfTanks">World Of Tanks</label>
-      <input type="radio" name="game" id="worldOfWarplanes" className={styles.inputRadio} defaultChecked />
-      <label className={styles.labelRadio} htmlFor="worldOfWarplanes">World Of Warplanes</label>
-      <input type="radio" name="game" id="worldOfWarships" className={styles.inputRadio} />
-      <label className={styles.labelRadio} htmlFor="worldOfWarships">World Of Warships</label>
-    </div>
-    <div className={styles.select}>
-      <span className={styles.menuIcon}>&#9776;</span>
-      <select name="selectGame" id="selectGame">
-        <option value="worldOfTanks">World Of Tanks</option>
-        <option value="worldOfWarplanes">World Of Warplanes</option>
-        <option value="worldOfWarships">World Of Warships</option>
-      </select>
-    </div>
-  </main>
-);
+const AdaptiveListComponent = () => {
+  const [selectedValue, setValue] = useState('worldOfWarplanes');
+  const [openSelect, setSelectOpen] = useState(false);
+
+  const chooseRadio = (event) => {
+    setValue(event.target.id);
+  };
+
+  const chooseGame = (event) => {
+    setValue(event.target.getAttribute('data-game'));
+    setSelectOpen(false);
+  };
+
+  const listOfGames = []; const selectOfGames = [];
+
+  Object.keys(games).forEach((el) => {
+    listOfGames.push(
+      <Fragment key={`${el}list`}>
+        <input
+          type="radio"
+          name="game"
+          id={el}
+          value={games[el]}
+          className={styles.inputRadio}
+          checked={selectedValue === el}
+          onChange={chooseRadio.bind(this)}
+        />
+        <label className={styles.labelRadio} htmlFor={el}>{games[el]}</label>
+      </Fragment>,
+    );
+    selectOfGames.push(
+      <Fragment key={`${el}select`}>
+        <span data-game={el} className={styles.option}>{games[el]}</span>
+      </Fragment>,
+    );
+  });
+
+  return (
+    <main className={styles.mainContent}>
+      <h1>Adaptive list</h1>
+      <div className={styles.listWrapper}>
+        {listOfGames}
+      </div>
+      <div className={styles.selectWrapper}>
+        <span className={styles.menuIcon}>&#9776;</span>
+        <button
+          type="button"
+          className={styles.select}
+          onClick={() => setSelectOpen(!openSelect)}
+        >
+          {games[selectedValue]}
+        </button>
+        {openSelect && (
+          <div className={styles.optionsWrapper} onClick={chooseGame.bind(this)}>
+            {selectOfGames}
+          </div>
+        )}
+      </div>
+    </main>
+  );
+};
 
 export default AdaptiveListComponent;
---
+++ src/utils/utils.js
@@ -0,0 +1,6 @@
+/* eslint-disable import/prefer-default-export */
+export const games = {
+  worldOfTanks: 'World Of Tanks',
+  worldOfWarplanes: 'World Of Warplanes',
+  worldOfWarships: 'World Of Warships',
+};
